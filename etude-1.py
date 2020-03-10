abc2b1f0020ff52c8236d280d657cf38707eb2a3
Darcy Knox <darcy@cloudcannon.com>
2020-03-10 16:02:34 +1300

    Tidying

--- etude-1.py
+++ etude-1.py
@@ -1,11 +1,6 @@
 import re
 import sys
 
-# Evaluate when IGNORECASE should be used
-# Is it only for mailbox names, or can it be for ext/domain as well?
-
-
-# working
 def matchMailbox(str):
     validMailboxPattern = re.compile(r'^[A-Z0-9]+([-_\.]?[A-Z0-9]+)*$', re.IGNORECASE)
     match = validMailboxPattern.match(str)
@@ -13,14 +8,14 @@ def matchMailbox(str):
     return bool(match)
 
 
-# working
 def matchAt(str):
     validAt = re.compile(r'(@|_at_)')
     match = validAt.search(str)
 
     return bool(match)
 
-def findAt(str):
+
+def findAtPos(str):
 
     nonSymbol = str.rfind('_at_')
     symbol = str.rfind('@')
@@ -33,21 +28,12 @@ def findAt(str):
         return None
 
 
-# working
-# domains finish with a dot
 def matchDomain(str):
     validDomain = re.compile(r'^([A-Z0-9]+((\.)?[A-Z0-9]+)*)+(\.|_dot_)$', re.IGNORECASE)
     match = validDomain.match(str)
 
     return bool(match)
 
-
-
-'''def isIPDomain(str):
-    validIP = re.compile(r'\[([0-9]+((\.)?[0-9]+)*)\]$')
-    match = validIP.search(str)
-
-    return bool(match)'''
 
 def isIPDomain(str):
     validIP = re.compile(r'\[(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9]\.){2}25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9]\]$')
@@ -56,10 +42,8 @@ def isIPDomain(str):
     return bool(match)
 
 
-# Maybe return the extension if it's not valid
 def matchExt(str):
     validExt = re.compile(r'(co\.nz|com\.au|co\.uk|com|co\.us|co\.ca)$', re.IGNORECASE)
-    #maybe don't use IGNORECASE here
     match = validExt.search(str)
 
     return bool(match)
@@ -70,6 +54,7 @@ def matchDomainAndExt(str):
     match = validDomainAndExt.match(str)
 
     return bool(match)
+
 
 def containsWhitespace(str):
     whitespace = re.compile(' ')
@@ -90,18 +75,13 @@ def fullMatch(str):
     #fullValidEmailRegex = re.compile(r'^([A-Z0-9]+([-_\.]?[A-Z0-9]+)*)+(@|_at_)([A-Z0-9]+((\.)?[A-Z0-9]+)*)+(\.|_dot_)(co\.nz|com\.au|co\.uk|com|co\.us|co\.ca)$', re.IGNORECASE)
     #match = fullValidEmailRegex.match(str)
 
-    '''
-    elif (str[-5:] == "co.nz" or str[-5:] == "co.uk" or str[-5:] == "co.ca" or str[-5:] == "co.us"):
-        print "Other valid ext"
-    '''
-
     print
 
-    if (containsWhitespace(str)):
+    if containsWhitespace(str):
         print (str + " <- Address contains whitespace")
         return
 
-    if (matchExt(str)):
+    if matchExt(str):
         if (str[-3:] == "com"):
             if (str[-8:-3] == "_dot_"):
                 #print "Uses _dot_ correctly"
@@ -121,64 +101,52 @@ def fullMatch(str):
                 str = str[:-10] + "." + str[-5:] #replace _dot_ with .
                 #print str
 
-    if matchAt(str):
-        if str[findAt(str)] == "@":
-            atIndex = findAt(str)
-        elif str[findAt(str):findAt(str) + 4] == "_at_":
-            str = str[:findAt(str)] + "@" + str[(findAt(str) + 4):]
-            atIndex = findAt(str)
 
-        '''print "Formatted @ symbol"
-        print str
-        print "atIndex"
-        print atIndex'''
+    if not matchAt(str):
+        print (str + " <- Missing @ symbol")
+        return
+    else:
+        if str[findAtPos(str)] == "@":
+            atIndex = findAtPos(str)
+        elif str[findAtPos(str):findAtPos(str) + 4] == "_at_":
+            str = str[:findAtPos(str)] + "@" + str[(findAtPos(str) + 4):]
+            atIndex = findAtPos(str)
 
-    # Need to make it clear how the domain is identified relative to the @ symbol
-
-    # then split at the valid @ symbol
-    # then check the mailbox and domain(s)
-
-
-    if matchAt(str):
-        #yes @ symbol exists
-
-        splitAddress = re.split('(@|_at_)', str) #split at the @ symbol
-        #print splitAddress
+        splitAddress = re.split('(@)', str) #split at the @ symbol
 
         if len(splitAddress) > 3: #if there are more than 3 parts
-
             print (str + " <- Too many @ symbols")
             return
-            #return str + error_message
 
         #separate string into 3 parts
         mailbox = splitAddress[0] #mailbox is the part before the @ symbol
         atSymbol = splitAddress[1] #atSymbol is the @ symbol
         domainAndExt = splitAddress[2] #domainAndExt is the part after the @symbol
 
-        #need a case for missing mailbox
+        #mailbox doesn't fit the regex
         if not matchMailbox(mailbox):
             consecutiveSeparators = re.compile(r'(\.|-|_)(\.|-|_)')
             if bool(consecutiveSeparators.search(mailbox)):
                 print (str + " <- Mailbox contains consecutive separators") #cannot contain consecutive separators (mailbox)
                 return
+            elif len(mailbox) == 0:
+                print (str + " <- Missing mailbox")
+                return
             else:
                 print (str + " <- Invalid mailbox")
                 return
-                #return str + error_message
 
         if not matchDomainAndExt(domainAndExt): #if there is an error in the domain or extension
-            consecutiveDots = re.compile(r'(\.|_dot_)(\.|_dot_)')
-            #this block might fit better in an outer condition so it catches consecutive dots in a mailbox as well
+            consecutiveDots = re.compile(r'(\.|_dot_)(\.|_dot_)') #looks for two dots next to eachother
             if bool(consecutiveDots.search(domainAndExt)):
                 print (str + " <- Domain contains consecutive separators") #cannot contain consecutive separators (domain)
                 return
 
-            #if matchExt(domainAndExt): #if there is a valid extension
+            #evaluate the extension first
             validExt = re.compile(r'(co\.nz|com\.au|co\.uk|com|co\.us|co\.ca)$', re.IGNORECASE)
             domainAndExtSplit = re.split(validExt, domainAndExt) #split at the extension
 
-            #says if there is an invalid extension (extension is not split)
+            #if there is an invalid extension (extension doesn't split)
             if len(domainAndExtSplit) < 2:
                 dotSeparator = re.compile(r'[A-Z0-9]\.[A-Z0-9]', re.IGNORECASE)
                 #says if there are no two characters separated by a dot, there must be a missing extension
@@ -193,32 +161,23 @@ def fullMatch(str):
                 ext = domainAndExtSplit[1] #second part is the extension
 
                 if not matchDomain(domain): #if the domain doesn't match the regex
-                    if not isIPDomain(domainAndExt): #if the domain and extension is not an IP
+                    if len(domain) == 0 or domain == ".": #if the domain is a dot or nothing
+                        print (str + " <- Missing domain")
+
+                    elif not isIPDomain(domainAndExt): #if the domain and extension is not an IP
                         print (str + " <- Invalid domain") #the domain is invalid
                         return
-                        #return str + error_message
 
-                #nested in the invlaid domain or extension
-                #the domainAndExt isn't an IP
-
-    #change to if not matchAt, else for better readability
-    else:
-        #no @ symbol
-        print (str + " <- Missing @ symbol")
-        return
-        #return str + error_message
-
-    # If the _dot_ couldn't be reaplaced anywhere else other thatn before the extension
+    # If the _dot_ couldn't be replaced anywhere else other than before the extension
     # this would need to change (remove replace dot and use the string indexing)
-    print (str.replace('_dot_', '.').lower())
+    # _dot_ can be anywhere that a . can be, but there can only be one _at_|@
+    print (str.replace('_dot_', '.').lower()) #replace the _dot_s
     return
 
 
 def main():
 
-    '''userIn = input()
-    fullMatch(userIn)'''
-
+    # User Input
     for line in sys.stdin:
         line = line.strip()
         fullMatch(line)
