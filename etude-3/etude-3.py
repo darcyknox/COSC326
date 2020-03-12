4240cfc81609bb5e2bf4429220effaaa199b76cc
Darcy Knox <38637487+darcyknox@users.noreply.github.com>
2020-03-13 11:07:51 +1300

    Add files via upload

---
+++ etude-3/etude-3.py
@@ -0,0 +1,48 @@
+import sys
+
+
+def add(x, y):
+    return x + y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def calculate(arr, target):
+
+    ops = [add, multiply]
+
+    # This only works for an array of 3 numbers
+    for i in range(2):
+        for j in range(2):
+            result = (ops[j](arr[2], (ops[i](arr[0], arr[1]))))
+            if result == target:
+                print (str(arr) + " " + ops[i].__name__ + " " + ops[j].__name__)
+                return
+
+def leftCalc(i, max_depth, currentVal, currentExpression, target, arr):
+
+    if(i == max_depth || currentVal == target):
+        return currentVal
+    else:
+        
+
+
+
+
+def main():
+
+    # User Input
+    '''count = 0
+    for line in sys.stdin:
+        if count % 2 == 0:
+            print("Order & target ")'''
+
+    calculate([1, 2, 3], 5)
+    calculate([1, 2, 3], 9)
+
+
+
+if __name__ == "__main__":
+    main()
